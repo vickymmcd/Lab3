ce2a008aa8dfba8d9276c7eaed07e45f763998de
vickymmcd <vickymmcd@gmail.com>
2018-10-27 18:10:11 -0400

    working on cpu a little more

--- cpu.v
+++ cpu.v
@@ -12,7 +12,10 @@ module CPU
   wire carryout, zero, overflow;
   alu pcalu1(PCplusfour, carryout, zero, overflow, PC, 32'd4, 3'b000);
   signextend signextend(immediate, clk, extendedimm);
+  // my attempt to shift it left by 2, might need modification
+  extendedimm <= {extendedimm 2'b00};
   alu pcalu2(PCfourimm, carryout, zero, overflow, PCplusfour, extendedimm, 3'b000);
+  // need to figure out how to use a mux to decide between different PC values
 
 
 endmodule
