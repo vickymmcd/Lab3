869b41f4f3b81800f8cd8f4fd9a2724d9dc1e50b
vickymmcd <vickymmcd@gmail.com>
2018-10-27 18:06:20 -0400

    working on cpu design

--- cpu.v
+++ cpu.v
@@ -1,11 +1,18 @@
 `include "alu.v"
+`include "signextend.v"
 
 module CPU
 (
+  input[16:0] immediate,
+  input clk,
+  input[31:0] PC,
   // insert inputs and outputs right here
 );
-  wire[31:0] PC, PCplusfour;
+  wire[31:0] PCplusfour, extendedimm;
   wire carryout, zero, overflow;
-  alu pcalu(PCplusfour, carryout, zero, overflow, PC, 32'd4, 3'b000);
+  alu pcalu1(PCplusfour, carryout, zero, overflow, PC, 32'd4, 3'b000);
+  signextend signextend(immediate, clk, extendedimm);
+  alu pcalu2(PCfourimm, carryout, zero, overflow, PCplusfour, extendedimm, 3'b000);
+
 
 endmodule
