4429a11ccaaf1772d827aaca601ef28292f831b3
Vicky McDermott <vickymmcd@gmail.com>
2018-10-30 13:05:22 -0400

    Fixing up assembly test

--- asmtest/JumpingFoxes/jalmem.asm
+++ asmtest/JumpingFoxes/jalmem.asm
@@ -2,16 +2,17 @@
 
 start:
 	li $t1, 3
-	li $t2, 0
-	# store the word in register $t1 into RAM at address contained in $t2-100
-	sw $t1, -100($t2)
+	# set the value of $sp to be the top of the stack
+	li $sp, 0x3ffc
+	# store the word in register $t1 into RAM at address contained in $sp+4
+	sw $t1, 4($sp)
 	jal get_val
 	# add 5 to the val loaded in from memory
 	addi $a0, $t3, 5
 	j end
 	
 get_val:
-	lw $t3, -100($t2)
+	lw $t3, 4($sp)
 	# jump back to where we were before jal
 	jr $ra
 	
