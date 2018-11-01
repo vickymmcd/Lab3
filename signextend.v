372edc2c829225d7e131ba61c37771b4f0814e32
Computer Architecture <allisonlynnbasore14@gmail.com>
2018-10-31 21:32:29 -0400

    fixed sign extend to not have a clock

--- signextend.v
+++ signextend.v
@@ -2,12 +2,11 @@ module signextend
 #(parameter width = 15)
 (
   input [width:0] unextended,
-  input clk,
   output reg [31:0] extended, shifted
 );
 
-always@(posedge clk)
-  begin 
+
+  initial begin 
     extended <= $signed(unextended);
     shifted <= extended<<2;
   end
