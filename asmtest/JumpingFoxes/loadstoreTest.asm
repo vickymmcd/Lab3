9ff7ca133646c3dacf20297a951c28778f4da84f
Computer Architecture <allisonlynnbasore14@gmail.com>
2018-11-04 21:25:38 -0500

    added back original test

--- asmtest/JumpingFoxes/loadstoreTest.asm
+++ asmtest/JumpingFoxes/loadstoreTest.asm
@@ -3,9 +3,7 @@ start:
 addi $a0, $zero, 2
 addi $a1, $zero, 4
 jal second
-
 addi $v0 $v0 10
-
 j end
 
 third:
@@ -13,18 +11,15 @@ add $v0, $a0, $a1
 jr $ra
 
 second:
-addi  $sp, $sp, -16	# Allocate three words on stack at once for three pushes
-sw    $ra, 8($sp)	# Push ra on the stack (will be overwritten by Fib function calls)
-sw    $s0, 4($sp)	# Push s0 onto stack
-sw    $s1, 0($sp)	# Push s1 onto stack
+sw    $ra, 3($sp)
+sw    $s0, 4($sp)
+sw    $s1, 5($sp)
+sw    $s1, 6($sp)
 
 addi $a2, $a0, 5
 jal third
 
-lw    $s1, 0($sp)	# Pop s1 from stack
-lw    $s0, 4($sp)	# Pop s0 from stack
-lw    $ra, 8($sp)	# Pop ra from the stack so we can return to caller
-addi  $sp, $sp, 12	# Adjust stack pointer to reflect pops
+lw    $s1, 0($sp)	
 
 addi $v0 $v0 3 
 jr $ra
